81ac797f1304834166eb3942072467f8d2c20643
juan_charry <juan.gonzalez@enmediolabs.com>
2026-05-29 14:54:17 -0500

    feat: exponer postProduct en servicio Create delegando a la API

--- src/app/core/services/createProduct/create.ts
+++ src/app/core/services/createProduct/create.ts
@@ -1,9 +1,13 @@
 import { inject, Injectable } from '@angular/core';
-import { Products } from '../api/products/products';
+import { CreateProduct, Product, Products } from '../api/products/products';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Create {
   private createProduct = inject(Products);
+  postProduct(product: CreateProduct): Observable<Product> {
+    return this.createProduct.postProduct(product);
+  }
 }
