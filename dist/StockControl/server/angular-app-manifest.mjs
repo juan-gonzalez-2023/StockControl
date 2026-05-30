c58d2d9743c12f51c280577d75b6d42a1e8e2aeb
juan_charry <juan.gonzalez@enmedio.com>
2026-05-30 16:47:53 -0500

    deploy: actualización de la aplicación en producción

--- dist/StockControl/server/angular-app-manifest.mjs
+++ dist/StockControl/server/angular-app-manifest.mjs
@@ -15,7 +15,7 @@ export default {
   {
     "renderMode": 1,
     "preload": [
-      "chunk-NDJ5SDUV.js"
+      "chunk-JFGSI3ZU.js"
     ],
     "route": "/home"
   },
@@ -27,8 +27,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 28522, hash: '9a4e729dbdcb774f5c808f811efe98f088d4d3f42c068a4be9b160d77ba91112', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 20960, hash: '18411dcdeb8922597311f16e82e5d41b6e9a37ae390b8a6d62f36af4d2b01997', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 28522, hash: '0451149bc3d49a82889109afb787fb2a510d3b88db12023b5a30c42cb3c644f9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 20960, hash: '4ca787acffd411973ec00b683479f4fb35883bff0ea6b81bfced110b1ed3e34b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-YPKFWAKN.css': {size: 8116, hash: 'gOIXaI3MvxA', text: () => import('./assets-chunks/styles-YPKFWAKN_css.mjs').then(m => m.default)}
   },
 };
